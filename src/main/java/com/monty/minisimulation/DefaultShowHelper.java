714b220bf785d6e4c39b9bd67b9dcbc0b268e7f7
Mikko Östlund <mikost@server.fake>
2015-01-09 11:53:00 +0100

    Removed commented-out code.

--- src/main/java/com/monty/minisimulation/DefaultShowHelper.java
+++ src/main/java/com/monty/minisimulation/DefaultShowHelper.java
@@ -6,7 +6,7 @@ public class DefaultShowHelper implements ShowHelper {
 
 	Random r = new Random();
 	public Doors setupDoors(int numberOfDoors) {
-		int indexOfDoorWithCar = r.nextInt(numberOfDoors);//(int)(Math.random() * numberOfDoors);
+		int indexOfDoorWithCar = r.nextInt(numberOfDoors);
 
 		return new Doors("dummy", indexOfDoorWithCar, numberOfDoors);
 	}
